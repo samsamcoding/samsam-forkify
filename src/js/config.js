4a73816039b14ffd816149dc31244ff98270535a
Samuel Dela Cruz <samsamcoding@gmail.com>
2022-04-26 22:08:49 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1e36e000-3640-4ec8-9b58-f0b33d3ee993';
+export const MODAL_CLOSE_SEC = 2.5;
